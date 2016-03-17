f087a469702a4cab1bbe9b6ebb2d09c2c413ef92
oangelo <oangelo@gmail.com>
2016-03-17 13:54:57 -0300

    Arduino code to get temperature and humidity

--- arduino/main.ino
+++ arduino/main.ino
@@ -1,69 +1,51 @@
-// Example testing sketch for various DHT humidity/temperature sensors
-// Written by ladyada, public domain
-
 #include "DHT.h"
 
 #define DHTPIN 2     // what digital pin we're connected to
-
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+char command[10]; //the command received by serial
+char aux_command[10]; // hold incomplete command
+unsigned cmd_count; //count the received chars
 
-// Initialize DHT sensor.
-// Note that older versions of this library took an optional third parameter to
-// tweak the timings for faster processors.  This parameter is no longer needed
-// as the current DHT reading algorithm adjusts itself to work on faster procs.
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("DHTxx test!");
-
   dht.begin();
+  //Serial.println("Home Automation System");
+  strncpy(command,"",10);
+  strncpy(aux_command,"",10);
+  cmd_count = 0;
+}
+
+void ReadCommand(){
+  if(Serial.available()){
+    char symbol = char(Serial.read());
+    if(symbol != '\n'){
+      aux_command[cmd_count]=symbol;
+      ++cmd_count;
+    }else{
+      command[cmd_count]='\0';
+      strncpy(command, aux_command, 10);
+      strncpy(aux_command,"",10);
+      cmd_count = 0;
+    }
+  }
+
 }
 
 void loop() {
-  // Wait a few seconds between measurements.
-  delay(2000);
-
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
-
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
+  delay(100);
+  ReadCommand();
+  //Execute a command
+  if(strcmp(command, "") != 0){
+    if(strcmp(command, "temp") == 0){
+      float t = dht.readTemperature();
+      Serial.println(t);
+    } else if(strcmp(command, "humidity") == 0){
+      float h = dht.readHumidity();
+      Serial.println(h);
+    }
+    strncpy(command,"",10);
   }
-
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
-
-  Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.print(" *C ");
-  Serial.print(f);
-  Serial.print(" *F\t");
-  Serial.print("Heat index: ");
-  Serial.print(hic);
-  Serial.print(" *C ");
-  Serial.print(hif);
-  Serial.println(" *F");
 }
